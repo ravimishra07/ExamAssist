99376950909888b647dfd1a1306f9a96361dde58
8anilkumar <anilkumarnishad920@gmail.com>
2021-05-05 12:40:18 +0530

    added log

--- app/src/main/java/com/ravi/examAssist/FirstFragment.kt
+++ app/src/main/java/com/ravi/examAssist/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.ravi.examAssist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,7 +24,7 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+   Log.v("firestfrag","onViewcreated called")
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
